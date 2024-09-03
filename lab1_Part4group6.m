5fb59e01ec3441ecec28b060a515872a68e66fb4
BurntIce16 <clayton.midgley@gmail.com>
2024-09-03 18:05:30 -0400

    fixed bug in lab1 q4 graphs

--- lab1_Part4group6.m
+++ lab1_Part4group6.m
@@ -5,18 +5,19 @@ C = A + B;
 D = C*[2;5;9];
 
 % Part B
-t = 0:10;
+figure;
+t = 0:0.01:10;
 y = sin(t);
 plot(t,y);
 hold on;
 title('Sin of 0 to 10 seconds');
 xlabel('Time');
 ylabel('sin()');
+hold off;
 
 % Part C
-
-x = meshgrid(-3:3);
-y = meshgrid(-1:5);
+figure;
+[x,y] = meshgrid(-3:0.1:3, -1:0.1:5);
 z = x.^2 + y.^2;
 surf(x,y,z);
 hold on;
@@ -24,9 +25,9 @@ title('Square of X + Square of Y');
 xlabel('Time');
 ylabel('sin()');
 zlabel('X^2 + Y^2');
+hold off;
 
 % Part D
 s = tf('s');
 func = s^4 + 3*s^3 - 15*s^2 - 2*s + 9;
-zero(func)
-
+roots = zero(func);
